da32999b49c688cdce9a7d24c6ae75d584eb9672
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-07-24 20:53:33 +0300

    5-dars

---
+++ js/index.js
@@ -0,0 +1,97 @@
+// Masalalar
+
+// 1-masala
+// let arr = [1, 2, 3, 4, 5];
+// let newArr = arr.map(num => num * 2);
+// console.log(newArr);
+
+
+// 4-masala
+// let arr = [1, 2, 3, 4, 5];
+// let sum = arr.reduce((acc, num) => acc + num, 0);
+// console.log(sum);
+
+// 5-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.push(6);
+// console.log(arr); 
+
+
+// 6-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.pop();
+// console.log(arr);
+
+// 7-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.shift();
+// console.log(arr);
+
+// 8-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.unshift(0);
+// console.log(arr);
+
+// 11-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// let max = Math.max(...arr);
+// console.log(max); 
+
+// 12-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// let min = Math.min(...arr);
+// console.log(min); 
+
+// 13-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// let countEven = arr.filter(num => num % 2 === 0).length;
+// console.log(countEven); 
+
+// 14-masala
+// let massiv = [1, 2, 3, 4, 5];
+// let toqSonlar = massiv.filter(son => son % 2 !== 0);
+// console.log(toqSonlar.length); 
+
+// 15-masala
+
+// let massiv = [0, 1, 2, 0, 3, 0];
+// let nolsizMassiv = massiv.filter(son => son !== 0);
+// console.log(nolsizMassiv); 
+
+// 16-masala
+// let massiv = [1, 2, 3];
+// let yangiMassiv = massiv.flatMap(son => [son, son]);
+// console.log(yangiMassiv); 
+
+// 18-masala
+
+// let massiv = [1, 2, 3, 4];
+// let kopaytma = massiv.reduce((a, b) => a * b, 1);
+// console.log(kopaytma); 
+
+// 19-masala
+
+// let massiv = [0, 1, 2, 3, 4, 5];
+// let toqIndeksli = massiv.filter((_, indeks) => indeks % 2 !== 0);
+// console.log(toqIndeksli);
+
+// 20-masala
+
+// let massiv = [0, 1, 2, 3, 4, 5];
+// let juftIndeksli = massiv.filter((_, indeks) => indeks % 2 === 0);
+// console.log(juftIndeksli);
+
+
+// 23-masala
+
+// let massiv = [1, 2, 3, 4, 5];
+// console.log(massiv.length);
+
+
